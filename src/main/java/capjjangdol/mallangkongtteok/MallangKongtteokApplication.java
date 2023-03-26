c66579ddaaa3ffdd0412ba9d676a3d0df1577358
bona <ra_ari@naver.com>
2023-03-27 01:12:01 +0900

    Initial commit

---
+++ src/main/java/capjjangdol/mallangkongtteok/MallangKongtteokApplication.java
@@ -0,0 +1,13 @@
+package capjjangdol.mallangkongtteok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallangKongtteokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallangKongtteokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/capjjangdol/mallangkongtteok/MallangKongtteokApplicationTests.java
@@ -0,0 +1,13 @@
+package capjjangdol.mallangkongtteok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallangKongtteokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
